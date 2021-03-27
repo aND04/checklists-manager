697c1897ba67300b8d7520199f9b7c93c96007c2
João Andrade <jm.andrade04@gmail.com>
2021-03-27 00:51:56 +0000

    .env environment variables integration, github action integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongoDbModule } from './config/MongoDbModule';
 import { ChecklistModule } from './checklist/checklist.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongoDbModule, ChecklistModule],
+  imports: [MongoDbModule, ChecklistModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/MongoDbModule.ts
+++ src/config/MongoDbModule.ts
@@ -1,8 +1,36 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { Injectable, Module } from '@nestjs/common';
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Injectable()
+class MongooseConfigService implements MongooseModuleOptions {
+  private readonly usr: string;
+  private readonly pw: string;
+  private readonly host: string;
+  private readonly port: number;
+  constructor(private configService: ConfigService) {
+    this.usr = this.configService.get<string>('DATABASE_USER');
+    this.pw = this.configService.get<string>('DATABASE_PASSWORD');
+    this.host = this.configService.get<string>('DATABASE_HOST');
+    this.port = this.configService.get<number>('DATABASE_PORT');
+  }
+
+  createMongooseOptions(): MongooseModuleOptions {
+    console.log(`mongodb://${this.usr}:${this.pw}@${this.host}:${this.port}`);
+    return {
+      uri: `mongodb://${this.usr}:${this.pw}@${this.host}:${this.port}`,
+    };
+  }
+}
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:root@localhost:27017')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: MongooseConfigService,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
